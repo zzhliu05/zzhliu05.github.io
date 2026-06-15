23c74b08304abeeb86a46384b4447612a3d1320d
zzhliu05 <zizhuangliu76@gmail.com>
2026-06-15 16:42:47 +0800

    0615 new article

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -140,7 +140,7 @@ export const siteConfig: SiteConfig = {
 			title: "zzhliu05 的博客",
 			switchable: true,
 
-			subtitle: ["Wir müssen wissen, wir werden wissen.","Like tears in rain."],
+			subtitle: ["All those moments will be lost in time,","Like tears in rain."],
 			typewriter: {
 				enable: true,
 
---
+++ src/content/posts/hopf-fiber.md
@@ -0,0 +1,9 @@
+---
+title: "Hopf Fiberation小记"
+description: "一篇短文"
+published: 2026-06-15
+tags: ["数学","拓扑物理"]
+image: "/images/posts/hopf-fiber.jpg"
+category: "拓扑"
+draft: false
+---
