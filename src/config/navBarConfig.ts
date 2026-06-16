9912c5c4eb5e8f478ac2257f722b8b56745a3b1c
zzhliu05 <zizhuangliu76@gmail.com>
2026-06-16 15:52:02 +0800

    Update navBarConfig.ts

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -5,12 +5,6 @@ export const navBarConfig: NavBarConfig = {
 	links: [
 		LinkPreset.Home,
 		LinkPreset.Archive,
-		{
-			name: "GitHub",
-			url: "https://github.com/zzhliu05",
-			external: true,
-			icon: "fa7-brands:github",
-		},
 		LinkPreset.Diary,
 		{
 			name: "GitHub",
